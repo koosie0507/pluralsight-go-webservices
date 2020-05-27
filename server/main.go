8d97a1dd7576193488312ddb47e151da945fbbef
Andrei Olar <andrei.olar@complyadvantage.com>
2020-05-27 16:41:22 +0300

    Clean up main.go

--- server/main.go
+++ server/main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/koosie0507/pluralsight-go-webservices/server/product"
 )
-
-func middlewareHandler(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("before handler; middleware start:", r.URL.Path, "Method:", r.Method)
-		start := time.Now()
-		handler.ServeHTTP(w, r)
-		fmt.Printf("middleware finished; %s\n", time.Since(start))
-	})
-}
 
 const apiBasePath = "/api"
 
